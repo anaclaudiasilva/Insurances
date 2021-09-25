8e827168fb7ae532835cf43249d93d165562f095
Ana Cláudia <claudia.2412@hotmail.com>
2021-09-25 11:13:56 -0300

    Add files via upload

---
+++ jsTeste/src/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  questoes: 'http://localhost:3000/get_response_ofertas_questoes',
+  residencial: 'http://localhost:3000/get_response_ofertas_residencial',
+  requestSimulacoesAssistenciais: 'http://localhost:3000/post_request_simulacoes_assistencias',
+  responseSimulacoesAssistenciais: 'http://localhost:3000/post_response_simulacoes_assistencias',
+};
